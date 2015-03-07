61db8f4b93be4df04376f99eb1bc036d6058e56d
TheGag96 <thegag96@gmail.com>
2015-03-06 19:20:33 -0600

    Maze generator translated to Python!

---
+++ server/game_app/maze.py
@@ -0,0 +1,162 @@
+import random
+
+SIZE = 25
+
+NORTH = 0
+EAST = 1
+SOUTH = 2
+WEST = 3
+
+class Wall:
+  def __init__(self, x, y, wallDir):
+    self.x = x
+    self.y = y
+    self.wallDir = wallDir
+    
+class BFSTile:
+  def __init__(self, x, y):
+    self.x = x
+    self.y = y
+    self.data = -1
+
+#implement breadth-first search. thanks again, wikipedia
+def getPathLength(map, startX, startY, endX, endY):
+  queue = []
+  tiles = [[BFSTile(x,y) for y in range(SIZE)] for x in range(SIZE)] 
+  
+  tiles[startX][startY].data = 4
+  queue.append(tiles[startX][startY])
+  
+  while len(queue) != 0:
+    v = queue[0]
+    del queue[0]
+    
+    if v.x == endX and v.y == endY:
+      break
+      
+    if v.x > 0 and map[v.x-1][v.y]:
+      if tiles[v.x-1][v.y].data == -1:
+        queue.append(tiles[v.x-1][v.y])
+        tiles[v.x-1][v.y].data = EAST
+    if v.y > 0 and map[v.x][v.y-1]:
+      if tiles[v.x][v.y-1].data == -1:
+        queue.append(tiles[v.x][v.y-1])
+        tiles[v.x][v.y-1].data = SOUTH
+    if v.x < SIZE-1 and map[v.x+1][v.y]:
+      if tiles[v.x+1][v.y].data == -1:
+        queue.append(tiles[v.x+1][v.y])
+        tiles[v.x+1][v.y].data = WEST
+    if v.y < SIZE-1 and map[v.x][v.y+1]:
+      if tiles[v.x][v.y+1].data == -1:
+        queue.append(tiles[v.x][v.y+1])
+        tiles[v.x][v.y+1].data = NORTH
+        
+  length = 0
+  runBack = tiles[endX][endY]
+  while runBack.data != 4:
+    if runBack.data == NORTH:
+      runBack = tiles[runBack.x][runBack.y-1]
+    if runBack.data == EAST:
+      runBack = tiles[runBack.x+1][runBack.y]
+    if runBack.data == SOUTH:
+      runBack = tiles[runBack.x][runBack.y+1]
+    if runBack.data == WEST:
+      runBack = tiles[runBack.x-1][runBack.y]
+    
+    length += 1
+  
+  return length
+        
+#generate maze using prim's algorithm. thanks wikipedia
+def generate():
+  map = [[False for y in range(SIZE)] for x in range(SIZE)] 
+  walls = []
+  
+  #choose starting position and add walls
+  firstX = random.randint(0,SIZE/2-1)*2+1;
+  firstY = random.randint(0,SIZE/2-1)*2+1;
+  map[firstX][firstY] = True
+  
+  if firstX > 1:
+    walls.append(Wall(firstX-1, firstY, WEST))
+  if firstY > 1:
+    walls.append(Wall(firstX, firstY-1, NORTH))
+  if firstX < SIZE-2:
+    walls.append(Wall(firstX+1, firstY, EAST))
+  if firstY < SIZE-2:
+    walls.append(Wall(firstX, firstY+1, SOUTH))    
+    
+  while len(walls) != 0:
+    #randomly choose wall from list
+    chosenIndex = random.randint(0,len(walls)-1);
+    chosenWall = walls[chosenIndex]
+    
+    otherSideIsInMaze = False
+    otherSideX = chosenWall.x
+    otherSideY = chosenWall.y
+    
+    #get tile on other side of wall
+    if chosenWall.wallDir == NORTH:
+      otherSideY -= 1
+    elif chosenWall.wallDir == EAST:
+      otherSideX += 1
+    elif chosenWall.wallDir == SOUTH:
+      otherSideY += 1
+    else:
+      otherSideX -= 1
+    otherSideIsInMaze = map[otherSideX][otherSideY]
+    
+    #break down wall to other side
+    if not otherSideIsInMaze:
+      map[otherSideX][otherSideY] = True
+      map[chosenWall.x][chosenWall.y] = True
+      
+      #add surrounding walls to wall list
+      if otherSideX > 1 and chosenWall.wallDir != 1 and not map[otherSideX-1][otherSideY]:
+        walls.append(Wall(otherSideX-1, otherSideY, WEST))
+      if otherSideY > 1 and chosenWall.wallDir != 2 and not map[otherSideX][otherSideY-1]:
+        walls.append(Wall(otherSideX, otherSideY-1, NORTH))
+      if otherSideX < SIZE-2 and chosenWall.wallDir != 3 and not map[otherSideX+1][otherSideY]:
+        walls.append(Wall(otherSideX+1, otherSideY, EAST))
+      if otherSideY < SIZE-2 and chosenWall.wallDir != 0 and not map[otherSideX][otherSideY+1]:
+        walls.append(Wall(otherSideX, otherSideY+1, SOUTH))
+        
+    del walls[chosenIndex]
+  
+  #build list of dead-ends (3+ adjacent walls)
+  deadEndList = []
+  for x in range(1, SIZE-1):
+    for y in range(1, SIZE-1):
+      if not map[x][y]:
+        continue
+        
+      adjacentWallCount = 0
+      
+      if x > 0 and not map[x-1][y]:
+        adjacentWallCount += 1
+      if y > 0 and not map[x][y-1]:
+        adjacentWallCount += 1
+      if x < SIZE-1 and not map[x+1][y]:
+        adjacentWallCount += 1
+      if y < SIZE-1 and not map[x][y+1]:
+        adjacentWallCount += 1
+        
+      if adjacentWallCount >= 3:
+        deadEndList.append(Wall(x,y,0))
+        
+  for tile in deadEndList:
+    if tile.x > 2 and getPathLength(map, tile.x, tile.y, tile.x-2, tile.y) >= SIZE:
+      map[tile.x-1][tile.y] = True
+    if tile.y > 2 and getPathLength(map, tile.x, tile.y, tile.x, tile.y-2) >= SIZE:
+      map[tile.x][tile.y-1] = True
+    if tile.x < SIZE-3 and getPathLength(map, tile.x, tile.y, tile.x+2, tile.y) >= SIZE:
+      map[tile.x+1][tile.y] = True
+    if tile.y < SIZE-3 and getPathLength(map, tile.x, tile.y, tile.x, tile.y+2) >= SIZE:
+      map[tile.x][tile.y+1] = True
+      
+  map[0][SIZE/2+1] = True
+  map[SIZE/2+1][SIZE-1] = True
+  map[SIZE-1][SIZE/2-1] = True
+  map[SIZE/2-1][0] = True
+  
+  return map
